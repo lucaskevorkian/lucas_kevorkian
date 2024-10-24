433eba913c8f72df15b4bfc5a5dfc2a54fbd70d1
Lucas Kevorkian <lucasfkevorkian@gmail.com>
2024-10-24 16:16:04 -0300

    avatar

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,14 +1,29 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 
 class formulario_creacion_usuario(UserCreationForm): #cambios a lo q viene de farica
     email = forms.EmailField()
+    first_name = forms.CharField(label="Nombre")
+    last_name = forms.CharField(label="Apellido")
     password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Repetir ontraseña", widget=forms.PasswordInput)
     
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "email", "first_name", "last_name", "password1", "password2"]
         help_texts = {key: "" for key in fields}
+        
+        
+        
+class formulario_edicion_perfil(UserChangeForm):
+    email = forms.EmailField(label="Email")
+    first_name = forms.CharField(label="Nombre")
+    last_name = forms.CharField(label="Apellido")
+    password = None
+    avatar = forms.ImageField(required=False)
+    
+    class Meta():
+        model = User
+        fields = ["email", "first_name", "last_name", "avatar"] 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django.contrib.auth import authenticate, login as django_login #es para poder usar el nombre login y darle 2 argumentos
-from .forms import formulario_creacion_usuario
+from .forms import formulario_creacion_usuario, formulario_edicion_perfil
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
+from .models import DatosExtra
+
+
+
 def login(request):
-    
     formulario = AuthenticationForm()
 
     if request.method == "POST":   #va por que es un form que va por post
@@ -16,6 +23,9 @@ def login(request):
             usuario = formulario.get_user()
             
             django_login(request, usuario)
+            
+            DatosExtra.objects.get_or_create(user=usuario)
+            
             return redirect("inicio")
     
     return render(request, "usuarios/login.html" , {"form": formulario})
@@ -33,3 +43,26 @@ def register(request):
             return redirect("usuarios:login")
     
     return render(request, "usuarios/register.html", {"form": formulario})
+
+
+@login_required
+def editar_perfil(request):
+    datos_extra = request.user.datosextra
+    formulario = formulario_edicion_perfil(instance=request.user, initial={"avatar": datos_extra.avatar})
+    
+    if request.method == "POST":
+        formulario = formulario_edicion_perfil(request.POST, request.FILES, instance=request.user)
+        if formulario.is_valid():
+            
+            new_avatar = formulario.cleaned_data.get("avatar")
+ 
+            datos_extra.avatar = new_avatar if new_avatar else datos_extra.avatar
+            datos_extra.save()
+            formulario.save()
+              
+            return redirect("inicio")
+    return render(request, "usuarios/editar_perfil.html", {"form": formulario})
+
+class cambiar_password(LoginRequiredMixin, PasswordChangeView):
+    template_name = "usuarios/cambiar_password.html"
+    success_url = reverse_lazy("usuarios:editar_perfil")
